1a550e01bb2543f8c8defdd254c0a18785ace793
Artjoms Lužnovs <greencomfytea@gmail.com>
2025-02-10 20:34:00 +0200

    upd workflow

--- build_vs2022.bat
+++ build_vs2022.bat
@@ -3,4 +3,5 @@ git submodule update --init --recursive
 mkdir build64_all
 cd build64_all
 cmake .. -G "Visual Studio 17 2022" -A x64 -DDEVELOPER_MODE=ON
-cmake --build . --config Release
+cmake --build . --config Release --target MHWILDS
+cmd /k
